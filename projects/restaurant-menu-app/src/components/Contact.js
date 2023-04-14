bc69f7c67d29a0f6cf202a95cc4fa6187b90ef96
Prashant Dey <prashantsavior@gmail.com>
2023-04-14 10:02:02 +0530

    WIP: controlled functional form

--- projects/restaurant-menu-app/src/components/Contact.js
+++ projects/restaurant-menu-app/src/components/Contact.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 function Contact() {
+    const [inputField , setInputField] = useState({
+        name: ''
+    })
+    const inputsHandler = (e) =>{
+        setInputField( {[e.target.name]: e.target.value} )
+    }
+    const submitButton = () =>{
+        alert(inputField.name)
+    }
     return (<>
+
         <form>
             <label>
                 Name:
-                <input type="text" name="name" />
+                <input type="text" 
+                name="name" 
+                onChange={inputsHandler} 
+                placeholder="Name"
+                value={inputField.name}
+                />
             </label>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={submitButton}/>
         </form>
     </>)
 }
